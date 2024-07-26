2ae3db3748322ac35ae87fa50edb62bbe58c8252
happycoder0011 <sheetalsinghnew@gmail.com>
2024-07-27 00:02:41 +0530

    feat: Test Setup with vitest done

--- src/components/Atoms/Checkbox/Checkbox.test.js
+++ src/components/Atoms/Checkbox/Checkbox.test.js
@@ -1,15 +1,26 @@
 import { unmountComponentAtNode } from "react-dom";
-
+import { afterEach, beforeEach } from 'vitest'
+import { describe, it, expect } from 'vitest';
 let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
+// beforeEach(() => {
+//   // setup a DOM element as a render target
+//   container = document.createElement("div");
+//   document.body.appendChild(container);
+// });
+
+describe('something truthy and falsy', () => {
+  it('true to be true', () => {
+    expect(true).toBe(true);
+  });
+
+  it('false to be false', () => {
+    expect(false).toBe(false);
+  });
 });
 
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
+// afterEach(() => {
+//   // cleanup on exiting
+//   unmountComponentAtNode(container);
+//   container.remove();
+//   container = null;
+// });
